ad40687ede6e22ac2bc32d83b9bb00f88413ba17
osborn.steven <osborn.steven@08f49e92-aade-11dd-9b86-41075523de00>
2008-11-10 01:27:44 +0000

    persistent lines

--- src/com/bitsetters/android/notes/NoteEdit.java
+++ src/com/bitsetters/android/notes/NoteEdit.java
@@ -18,14 +18,18 @@ package com.bitsetters.android.notes;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -34,195 +38,221 @@ public class NoteEdit extends Activity {
 	public static final String TAG = "NoteEdit";
 
 	public static final int DELETE_INDEX = Menu.FIRST;
-	
-	private NoteEntry note;
-	protected EditText edit_txt;	
-	
-    private DBHelper dbHelper=null;
+	public static final int EMAIL_INDEX = Menu.FIRST + 1;
 
-    public static class NoteHeader extends TextView {
+	private NoteEntry note;
+	protected EditText edit_txt;
+
+	private DBHelper dbHelper = null;
+
+	public static class NoteHeader extends TextView {
 		private Paint mLine;
 
-    	public NoteHeader(Context context, AttributeSet attrs) {
+		public NoteHeader(Context context, AttributeSet attrs) {
 			super(context, attrs);
-			
+
 			setPadding(20, 0, 0, 0);
-			
+
 			setHeight(20);
 			mLine = new Paint();
 			mLine.setStyle(Paint.Style.STROKE);
 			mLine.setARGB(100, 255, 0, 0);
-    	}
-       		
-    	   @Override
-           protected void onDraw(Canvas canvas) {               
-               Paint paint = mLine;
+		}
 
-               canvas.drawLine(getLeft(), getHeight() -1, getRight(), getHeight()  -1, paint);
-               canvas.drawLine(10, getTop(), 10, getBottom(), paint);
-               canvas.drawLine(15, getTop(), 15, getBottom(), paint);
- 
-               super.onDraw(canvas);
-           }    	
-    }
+		@Override
+		protected void onDraw(Canvas canvas) {
+			Paint paint = mLine;
 
-    
-    /**
-     * A custom EditText that draws lines between each line of text that is displayed.
-     */
-    public static class LinedEditText extends EditText {
-        private Rect mRect;
-        private Paint mPaint;
-        private Paint mVert;
+			canvas.drawLine(getLeft(), getHeight() - 1, getRight(),
+					getHeight() - 1, paint);
+			canvas.drawLine(10, getTop(), 10, getBottom(), paint);
+			canvas.drawLine(15, getTop(), 15, getBottom(), paint);
 
-        
-        // we need this constructor for LayoutInflater
-        public LinedEditText(Context context, AttributeSet attrs) {
-            super(context, attrs);
+			super.onDraw(canvas);
+		}
+	}
 
-            mRect = new Rect();
-            mPaint = new Paint();
-            mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setARGB(100, 0, 0, 255);
-            mVert = new Paint();
-            mVert.setStyle(Paint.Style.STROKE);
-            mVert.setARGB(100, 255, 0, 0);
-            setPadding(20, 0, 0, 0);
-        }
+	/**
+	 * A custom EditText that draws lines between each line of text that is
+	 * displayed.
+	 */
+	public static class LinedEditText extends EditText {
+		private Paint mPaint;
+		private Paint mVert;
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            int count = getLineCount();
-            
-            Rect r = mRect;
-            Paint paint = mPaint;
+		// we need this constructor for LayoutInflater
+		public LinedEditText(Context context, AttributeSet attrs) {
+			super(context, attrs);
 
-//            int height = getHeight();
-//            int line_height = getLineHeight();
-//            int count = height / line_height;
-//            
-//            for (int i=1; i< count; i++) {
-//            	int posY = i*line_height;
-//            	canvas.drawLine(getLeft(), posY + 1, getRight(), posY + 1, paint);
-//            }
-//            
-//        	canvas.drawLine(10, getTop(), 10, getBottom(), mVert);
-//        	canvas.drawLine(15, getTop(), 15, getBottom(), mVert);
+			mPaint = new Paint();
+			mPaint.setStyle(Paint.Style.STROKE);
+			mPaint.setARGB(100, 0, 0, 255);
+			mVert = new Paint();
+			mVert.setStyle(Paint.Style.STROKE);
+			mVert.setARGB(100, 255, 0, 0);
+			setPadding(20, 0, 0, 0);
+		}
 
-            
-            for (int i = 0; i < count; i++) {
-                int baseline = getLineBounds(i, r);
-
-                canvas.drawLine(r.left - 20, baseline + 1, r.right, baseline + 1, paint);
-                canvas.drawLine(10, r.top, 10, r.bottom, mVert);
-                canvas.drawLine(15, r.top, 15, r.bottom, mVert);
-            }
-
-            super.onDraw(canvas);
-        }
-    }
-
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
+		@Override
+		protected void onDraw(Canvas canvas) {
+			int count = getLineCount();
 	
-		menu.add(0,DELETE_INDEX, 0, R.string.delete)
-			.setIcon(android.R.drawable.ic_menu_delete)
-			.setShortcut('1', 'd');
+			Paint paint = mPaint;
+	
+			int height = getHeight();
+			int line_height = getLineHeight();
+			int page_size = height / line_height;
+			
+			if (count < page_size) {
+				count = page_size;
+			}
+			for (int i=1; i< count; i++) {
+				int posY = 0; 
+					 posY = i*line_height;
+				 canvas.drawLine(getLeft(), posY -1, getRight(), posY -1, paint);
+			}
+				
+			canvas.drawLine(10, 0, 10, getBottom(), mVert);
+			canvas.drawLine(15, 0, 15, getBottom(), mVert);
+			
+			super.onDraw(canvas);
+		}
+		
+	} //Class
+		
+	private void updateTitle() {
+		if (edit_txt != null) {
+			String title = edit_txt.getText().toString();
+			String first = title.split("\\n")[0];
+			if (first.length() > 30) {
+				first = first.substring(0, 27) + "...";
+			}
+			setTitle("Note - " + first);
+		}
+	}
+
+	private void sendEmail() {
+		Intent sendIntent = new Intent(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, edit_txt.getText().toString());
+		sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+		sendIntent.setType("message/rfc822");
+		startActivity(Intent.createChooser(sendIntent, "Title:"));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+
+		menu.add(0, EMAIL_INDEX, 0, R.string.email).setIcon(
+				android.R.drawable.ic_menu_send).setShortcut('1', 'e');
+		menu.add(0, DELETE_INDEX, 0, R.string.delete).setIcon(
+				android.R.drawable.ic_menu_delete).setShortcut('2', 'd');
 
 		return super.onCreateOptionsMenu(menu);
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case EMAIL_INDEX:
+			sendEmail();
+			break;
 		case DELETE_INDEX:
-		    deleteNote();
-		    finish();
-		    break;
+			deleteNote();
+			finish();
+			break;
 		}
 		return super.onOptionsItemSelected(item);
-    }
-    
-    @Override
-    public boolean onMenuOpened(int featureId, Menu menu) {
-    	menu.findItem(DELETE_INDEX).setEnabled(note.id >= 0);
-    	return super.onMenuOpened(featureId, menu);
-    }
-    
-    private void deleteNote() {
-    	dbHelper.deleteNote(note.id);
-    }
-	
-    private void save() {
+	}
+
+	@Override
+	public boolean onMenuOpened(int featureId, Menu menu) {
+		menu.findItem(DELETE_INDEX).setEnabled(note.id >= 0);
+		return super.onMenuOpened(featureId, menu);
+	}
+
+	private void deleteNote() {
+		dbHelper.deleteNote(note.id);
+	}
+
+	private void save() {
 		note.note = edit_txt.getText().toString();
 		if (note.id >= 0) {
 			// Editing an existing note
 			dbHelper.updateNote(note.id, note);
-		}
-		else {
+		} else {
 			// Creating a note
 			dbHelper.addNote(note);
 		}
-    }
-	    
-    @Override
-    protected void onPause() {
+	}
+
+	@Override
+	protected void onPause() {
 		super.onPause();
-		
-		Log.d(TAG,"onPause()");
+
+		Log.d(TAG, "onPause()");
 		if (dbHelper != null) {
 			save();
 			dbHelper = null;
 		}
-    }
-    
-    @Override
-    public void onStop() {
+	}
+
+	@Override
+	public void onStop() {
 		super.onStop();
-		Log.d(TAG,"onStop()");
+		Log.d(TAG, "onStop()");
 		if (dbHelper != null) {
 			save();
-			dbHelper=null;
+			dbHelper = null;
 		}
-    }
-    
-    @Override
-    protected void onResume() {
+	}
+
+	@Override
+	protected void onResume() {
 		super.onResume();
-		
-		Log.d(TAG,"onResume()");
+
+		Log.d(TAG, "onResume()");
 
 		if (dbHelper == null) {
-		    dbHelper = new DBHelper(this);
-		}
-    }
-
-	/**
-	 *  Called when the activity is first created.
-	 */
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.note_edit);
-        edit_txt = (EditText) findViewById(R.id.edit_note);
-        edit_txt.setLongClickable(true);
-        
-
-        if (dbHelper==null) {
 			dbHelper = new DBHelper(this);
 		}
-    	
-        int id = getIntent().getIntExtra("id", -1);
-        
-        if (id >= 0) {
-	        note = dbHelper.fetchNote(id);
-	        edit_txt.setText(note.note);
-        }
-        else {
-        	note = new NoteEntry();
-        	note.id = -1;
-        }
-    }
-    
+	}
+
+	OnKeyListener key_listener = new OnKeyListener() {
+
+		@Override
+		public boolean onKey(View v, int keyCode, KeyEvent event) {
+			updateTitle();
+			return false;
+		}
+
+	};
+
+	/**
+	 * Called when the activity is first created.
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.note_edit);
+		edit_txt = (EditText) findViewById(R.id.edit_note);
+		edit_txt.setLongClickable(true);
+		edit_txt.setOnKeyListener(key_listener);
+
+		if (dbHelper == null) {
+			dbHelper = new DBHelper(this);
+		}
+
+		int id = getIntent().getIntExtra("id", -1);
+
+		if (id >= 0) {
+			note = dbHelper.fetchNote(id);
+			edit_txt.setText(note.note);
+		} else {
+			note = new NoteEntry();
+			note.id = -1;
+		}
+		updateTitle();
+
+	}
+
 }
